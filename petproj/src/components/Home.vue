7d68f27f9193130154f27f5664a21cce59b88ce4
Tomiwa <ajaomoshood@gmail.com>
2025-01-08 09:36:20 +0100

    hompage initial message

--- petproj/src/components/Home.vue
+++ petproj/src/components/Home.vue
@@ -3,11 +3,22 @@
 </script>
 
 <template>
-  <h1>
-    Welcome to PetProj 😃
-  </h1>
+   <div class="container text-center ">
+        <div class="row justify-content-md-center">
+            <div class=" col col-lg-6   ">
+                <h2 class="text-primary">Find Your Perfect Pet Companion Today!</h2>
+                <h6 class=" text-subject text-primary">Discover adorable pets waiting to brighten your life. 
+                  Explore a wide range of healthy and happy companions from trusted breeders and shelters.
+
+                </h6>
+            </div>
+        </div>
+        <RouterLink to="/adopt" class="btn btn-primary btn-lg text-center my-4">Adopt a Pet</RouterLink>
+    </div>
 </template>
 
 <style scoped>
-
+  .container {
+    margin-top: 10%;
+  }
 </style>
--- petproj/src/components/NavBar.vue
+++ petproj/src/components/NavBar.vue
@@ -1,9 +1,9 @@
 <script setup></script>
 
 <template>
- <nav class="navbar navbar-expand-lg navbar-light bg-dark" data-bs-theme="dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Pet-Care</a>
+ <nav class="navbar navbar-expand-lg navbar-dark bg-dark" data-bs-theme="dark">
+  <div class="container-fluid w-100">
+    <a class="navbar-brand fw-bolder text-warning" href="#">Pet-Care</a>
     <button
       class="navbar-toggler"
       type="button"
@@ -15,28 +15,33 @@
     >
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+    <div class="collapse navbar-collapse justify-content-center my-8 mx-8" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
           <RouterLink class="nav-link active" aria-current="page" to="/">Home</RouterLink>
         </li>
         <li class="nav-item">
-          <RouterLink class="nav-link" to="/adopt">Adopt</RouterLink>
+          <RouterLink class="nav-link active" to="/adopt">Adopt</RouterLink>
         </li>
         <li class="nav-item">
-          <RouterLink class="nav-link" to="/volunteer">Volunteer</RouterLink>
+          <RouterLink class="nav-link active" to="/volunteer">Volunteer</RouterLink>
         </li>
         <li class="nav-item">
-          <RouterLink class="nav-link" to="/resources">Resources</RouterLink>
+          <RouterLink class="nav-link active" to="/resources">Resources</RouterLink>
         </li>
         <li class="nav-item">
-          <RouterLink class="nav-link" to="/about">About</RouterLink>
+          <RouterLink class="nav-link active" to="/about">About</RouterLink>
         </li>
       </ul>
-      <RouterLink to="/donate" class="btn btn-primary ms-auto">Donate</RouterLink>
+      <RouterLink to="/donate" class="btn btn-primary">Donate</RouterLink>
     </div>
   </div>
 </nav>
 
 </template>
+<style scoped>
 
+.navbar {
+  border-width: 40%;
+}
+</style>
