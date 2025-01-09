37fd8aa27ac48a201142fa0dcd18bbe215da5449
Tomiwa <ajaomoshood@gmail.com>
2025-01-09 16:19:05 +0100

    adjustments to landing page, footer next

--- petproj/src/components/Home.vue
+++ petproj/src/components/Home.vue
@@ -6,7 +6,7 @@
    <div class="container text-center ">
         <div class="row ">
             <div class=" col col-lg-6   ">
-                <h2 class=" title">Find Your Perfect Pet Companion Today !</h2>
+                <h2 class=" title text-warning">Find Your Perfect Pet Companion Today !</h2>
                 <h6 class=" text-subject  text-black">Discover adorable pets waiting to brighten your life. 
                   Explore a wide range of healthy and happy companions from trusted breeders and shelters.
 
@@ -18,25 +18,41 @@
           <RouterLink to="/donate" class="btn btn-outline-primary btn-lg text-center my-4">Donate</RouterLink>
         </div>
     </div>
+    /* Footer section *\
+    <div class="container-1">
+      <div class="row">
+        <div class="col col-md-6">
+          <h1 class="text-light justify-content-center">PET-CARE</h1>
+        </div>
+      </div>
+    </div>
 </template>
 
 <style scoped>
   
   .container {
-    margin-top: 8%;
+   
     margin-bottom: 18%;
+    background-image: url('../assets/img/dognew.png');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height:100vh;
+    width:100vw;
+    background-color: gray;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
   }
 
-  .title {
+    .title {
       font-family: Verdana, Geneva, Tahoma, sans-serif;
       font-weight: bold;
-      color: #b5c204;
     }
 
     .text-subject {
       font-family: Verdana, Geneva, Tahoma, sans-serif;
-      
+      font-size: 10px;      
     }
+   
+
     @media (min-width: 1024px)  {
     .container {
       
@@ -48,7 +64,6 @@
       font-weight: bold;
       width: 90%;
       text-align: start;
-      color: #b5c204;
       font-size: 50px;
     }
 
@@ -56,7 +71,6 @@
       font-family: Verdana, Geneva, Tahoma, sans-serif;
       text-align: start;
       width: 65%;
-      color: white;
       font-size: 22px;
     }
 
@@ -69,23 +83,22 @@
 
   @media (max-width: 430px)  {
     .container {
-      
-      margin-right: 17%;
+      background-size:contain;
+      margin-right: 35%;
       
     }
     .title {
       font-family: Verdana, Geneva, Tahoma, sans-serif;
       font-weight: bold;
-      width: 100%;
+      width: 73%;
       text-align: start;
-      color: #b5c204;
+      
     }
 
     .text-subject {
       font-family: Verdana, Geneva, Tahoma, sans-serif;
       text-align: start;
-      width: 73%;
-      color: white;
+      width: 53%;
     }
 
     .buttons {
@@ -106,20 +119,47 @@
       font-weight: bold;
       width: 60%;
       text-align: start;
-      color: #b5c204;
     }
 
     .text-subject {
       font-family: Verdana, Geneva, Tahoma, sans-serif;
       text-align: start;
-      width: 60%;
-      color: white;
+      width: 50%;
     }
 
     .buttons {
       text-align: start;
      
       max-width: 50%;
+    }
+  }
+
+
+    @media (min-width: 660px)  {
+    .container {
+      
+      text-align: left;
+      
+    }
+    .title {
+      font-family: Verdana, Geneva, Tahoma, sans-serif;
+      font-weight: bold;
+      width: 70%;
+      text-align: start;
+      font-size: 50px;
+    }
+
+    .text-subject {
+      font-family: Verdana, Geneva, Tahoma, sans-serif;
+      text-align: start;
+      width: 65%;
+      font-size: 22px;
+    }
+
+    .buttons {
+      text-align: start;
+     
+      max-width: 80%;
     }
   }
 </style>
